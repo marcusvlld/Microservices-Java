a1c1c1f072c87746e6d5ade0f63bfc38a4b69782
marcusvlld <marcusvld92@gmail.com>
2026-07-31 12:42:15 -0300

    feat(auth): initialize AuthService application and add Role entity

---
+++ services/auth-service/src/main/java/io/github/marcusvlld/orderflow/auth/AuthServiceApplication.java
@@ -0,0 +1,8 @@
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
